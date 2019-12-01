098f97e15c6bcd9418cc416f33b8324911cbf0b8
gautami <gjoshi2104@gmail.com>
2019-12-01 21:35:40 +0530

    Intitial git

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ProductListComponent } from './product-list/product-list.component';
+import { HttpClientModule } from '@angular/common/http';
+import {RouterModule} from '@angular/router'
+import { ProductService } from './product.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductListComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    RouterModule.forRoot([
+      {path:"product",component:ProductListComponent}])
+    
   ],
-  providers: [],
+  providers: [ ProductService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/product-list/product-list.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import { ProductService } from '../product.service';
+
+@Component({
+  selector: 'app-product-list',
+  templateUrl: './product-list.component.html',
+  styleUrls: ['./product-list.component.css']
+})
+export class ProductListComponent implements OnInit {
+
+  products:any;
+  constructor(private service:ProductService) { }
+
+
+  ngOnInit() {
+    let observableResult=this.service.Select();
+    observableResult.subscribe((result)=>{
+      this.products=result;
+    });
+  }
+
+}
